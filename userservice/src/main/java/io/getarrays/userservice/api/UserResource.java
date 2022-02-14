7e3674b67a26c716dbabc1e7d7f84a331f8ec923
hunojung <huno0504@naver.com>
2022-02-14 19:09:56 +0900

    Update

---
+++ userservice/src/main/java/io/getarrays/userservice/api/UserResource.java
@@ -0,0 +1,54 @@
+package io.getarrays.userservice.api;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+	private final UserService userService;
+	
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getUsers() {
+		return ResponseEntity.ok().body(userService.getUsers());
+	}
+	
+	@PostMapping("/user/save")
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toString());
+		return ResponseEntity.created(uri).body(userService.saveUser(user));
+	}
+	
+	@PostMapping("/role/save")
+	public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toString());
+		return ResponseEntity.created(uri).body(userService.saveRole(role));
+	}
+	
+	@PostMapping("/role/save")
+	public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+		userService.addRoleToUser(form.getUsername(), form.getRoleName();
+		return ResponseEntity.ok().build();
+	}
+}
+
+@Data
+class RoleToUserForm{
+	private String username;
+	private String roleName;
+	
+}
---
+++ userservice/src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package io.getarrays.userservice.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.repo.RoleRepo;
+import io.getarrays.userservice.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service @RequiredArgsConstructor @Transactional @Slf4j
+public class UserServiceImpl implements UserService{
+	private final UserRepo userRepo;
+	private final RoleRepo roleRepo;
+	
+	
+	public User saveUser(User user) {
+		// TODO Auto-generated method stub
+		log.info("Saving new user {} to the database",user.getName());
+		return userRepo.save(user);
+	}
+
+	public Role saveRole(Role role) {
+		// TODO Auto-generated method stub
+		log.info("Saving new role {} to the database",role.getName());
+		return roleRepo.save(role);
+	}
+
+	public void addRoleToUser(String username, String roleName) {
+		// TODO Auto-generated method stub
+		log.info("Adding role {} to user {}",roleName,username);
+		User user = userRepo.findByUsername(username);
+		Role role = roleRepo.findByName(roleName);
+		user.getRoles().add(role);
+	}
+
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		log.info("Fetching user {}",username);
+		return userRepo.findByUsername(username);
+	}
+
+	public List<User> getUsers() {
+		// TODO Auto-generated method stub
+		log.info("Fetching users");
+		return userRepo.findAll();
+	}
+}
